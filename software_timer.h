f927c03b1ef4d9ca91a687a06f534b260cfc9bd2
NPAnh1109 <104689229+NPAnh1109@users.noreply.github.com>
2023-09-26 17:59:27 +0700

    Add files via upload

---
+++ software_timer.h
@@ -0,0 +1,17 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Sep 18, 2023
+ *      Author: 84977
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer0_flag, timer1_flag;
+
+void setTimer0(int duration);
+void setTimer1(int duration);
+void timerRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
